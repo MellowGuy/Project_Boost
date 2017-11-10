8b7fd1fdbb1377427d5bfda8f51fd51d3b935f91
Tyler R Thompson <thotyl88@gmail.com>
2017-11-09 22:51:30 -0500

    added ship and thrust

---
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+	public float thrustSpeed = 5f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInput();
+	}
+
+	private void GetInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			//thrusting
+			rb.AddRelativeForce(Vector3.up);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			//rotate left
+
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			//rotate right
+
+		}
+	}
+}
