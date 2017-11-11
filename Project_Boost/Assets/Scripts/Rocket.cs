d786ddebfaf9df695e231234aacff8113457b276
Tyler R Thompson <thotyl88@gmail.com>
2017-11-11 16:41:35 -0500

    added metal materials, refactored some code

--- Project_Boost/Assets/Scripts/Rocket.cs
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 	public float thrustSpeed = 5f;
 	Rigidbody rb;
+	AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,21 +21,43 @@ public class Rocket : MonoBehaviour {
 
 	private void GetInput()
 	{
+		Thrust();
+		Rotate();
+	}
+
+	private void Thrust()
+	{
 		if (Input.GetKey(KeyCode.Space))
 		{
 			//thrusting
 			rb.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
 		}
+		else
+		{
+			audioSource.Stop();
+		}
+	}
 
+	private void Rotate()
+	{
+		rb.freezeRotation = true; // take manual control of rotation
 		if (Input.GetKey(KeyCode.A))
 		{
 			//rotate left
+			transform.Rotate(Vector3.forward);
 
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
 			//rotate right
+			transform.Rotate(-Vector3.forward);
 
 		}
+
+		rb.freezeRotation = false; //resume physics control of rotation
 	}
 }
